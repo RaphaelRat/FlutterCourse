b26d01c5ddb1a0769f85f929e4c681191ada031e
Raphael Abreu <raphaelrat@hotmail.com>
2022-02-22 10:41:49 -0300

    ensure HttpAdapter return null if post returns 200 with no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -16,7 +16,7 @@ class HttpAdapter implements HttpClient {
     final headers = {'content-type': 'aplication/json', 'accept': 'aplication/json'};
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers, body: jsonBody);
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -54,6 +54,14 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, {'any_key': 'any_value'});
+    });
+
+    test('Should return null if post returns 200 with no data', () async {
+      when(client.post(any, headers: anyNamed('headers'))).thenAnswer((_) async => Response('', 200));
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
     });
   });
 }
