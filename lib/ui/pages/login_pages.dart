487c69729ad14614655258946b7d84c2e9807af3
Raphael Abreu <raphaelrat@hotmail.com>
2022-02-24 08:57:41 -0300

    add LoginPage

---
+++ lib/ui/pages/login_pages.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../components/components.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            LoginHeader(),
+            Headline1('login'),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(Icons.email, color: Theme.of(context).primaryColorLight),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, bottom: 32),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          icon: Icon(Icons.lock, color: Theme.of(context).primaryColorLight),
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text('Entrar'.toUpperCase()),
+                    ),
+                    FlatButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.person),
+                      label: Text('Criar Conta'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
